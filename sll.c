4963ee39ba236a2a108966db44b8bf39fe3adbb5
dipamantre97 <87062425+dipamantre97@users.noreply.github.com>
2021-08-06 19:45:32 +0530

    Add files via upload

---
+++ sll.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+    int num;                        //Data of the node
+    struct node *nextptr;           //Address of the next node
+}*stnode;
+
+void createList(int n); // function to create the list
+void displayList();         // function to display the list
+
+int main()
+{
+    int n;
+		printf("\n\n Linked List : To create and display Singly Linked List :\n");
+		printf("-------------------------------------------------------------\n");
+		
+    printf(" Input the number of nodes : ");
+    scanf("%d", &n);
+    createList(n);
+    printf("\n Data entered in the list : \n");
+    displayList();
+    return 0;
+}
+void createList(int n)
+{
+    struct node *fnNode, *tmp;
+    int num, i;
+    stnode = (struct node *)malloc(sizeof(struct node));
+
+    if(stnode == NULL) //check whether the fnnode is NULL and if so no memory allocation
+    {
+        printf(" Memory can not be allocated.");
+    }
+    else
+    {
+
+
+        printf(" Input data for node 1 : ");
+        scanf("%d", &num);
+        stnode->num = num;      
+        stnode->nextptr = NULL; // links the address field to NULL
+        tmp = stnode;
+// Creating n nodes and adding to linked list
+        for(i=2; i<=n; i++)
+        {
+            fnNode = (struct node *)malloc(sizeof(struct node));
+            if(fnNode == NULL)
+            {
+                printf(" Memory can not be allocated.");
+                break;
+            }
+            else
+            {
+                printf(" Input data for node %d : ", i);
+                scanf(" %d", &num);
+ 
+                fnNode->num = num;      
+                fnNode->nextptr = NULL; 
+ 
+                tmp->nextptr = fnNode; 
+                tmp = tmp->nextptr; 
+            }
+        }
+    }
+}
+void displayList()
+{
+    struct node *tmp;
+    if(stnode == NULL)
+    {
+        printf(" List is empty.");
+    }
+    else
+    {
+        tmp = stnode;
+        while(tmp != NULL)
+        {
+            printf(" Data = %d\n", tmp->num);       
+            tmp = tmp->nextptr;                     
+        }
+    }
+} 
+/*
+ Linked List : To create and display Singly Linked List :
+-------------------------------------------------------------
+ Input the number of nodes : 3
+ Input data for node 1 : 100
+ Input data for node 2 : 200
+ Input data for node 3 : 300
+
+ Data entered in the list :
+ Data = 100
+ Data = 200
+ Data = 300 */
