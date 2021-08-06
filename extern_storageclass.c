667108ab923e67fca896d81aaa150d5c66ada3c3
dipamantre97 <87062425+dipamantre97@users.noreply.github.com>
2021-08-06 19:41:39 +0530

    Add files via upload

---
+++ extern_storageclass.c
@@ -0,0 +1,16 @@
+//variable.h
+//extern int num1 = 9;
+//extern int num2 = 1;
+
+
+extern.c
+#include <stdio.h>
+#include "variable.h"
+int main()
+{
+int add = num1 + num2;
+printf("%d + %d = %d ", num1, num2, add);
+return 0;
+}
+
+
