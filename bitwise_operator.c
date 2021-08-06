9d67c9dfe653ef02fdcd2b917070002b93a4d90e
dipamantre97 <87062425+dipamantre97@users.noreply.github.com>
2021-08-06 19:38:16 +0530

    Add files via upload

---
+++ bitwise_operator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main() {
+
+   unsigned int a = 60;	/* 60 = 0011 1100 */  
+   unsigned int b = 13;	/* 13 = 0000 1101 */
+   int c = 0;           
+
+   c = a & b;       /* 12 = 0000 1100 */ 
+   printf(" Value of c is %d\n", c );
+
+   c = a | b;       /* 61 = 0011 1101 */
+   printf(" Value of c is %d\n", c );
+
+   c = a ^ b;       /* 49 = 0011 0001 */
+   printf(" Value of c is %d\n", c );
+
+   c = ~a;          /*-61 = 1100 0011 */
+   printf(" Value of c is %d\n", c );
+
+   c = a << 2;     /* 240 = 1111 0000 */
+   printf("Value of c is %d\n", c );
+
+   c = a >> 2;     /* 15 = 0000 1111 */
+   printf(" Value of c is %d\n", c );
+}
